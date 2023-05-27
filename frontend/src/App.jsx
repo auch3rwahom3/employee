7a5afcc67d3c6dc57bda1af262554d9d2e02bf0c
auch3rwahom3 <aucherwahome@gmail.com>
2023-05-26 20:49:11 -0700

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import Login from './Login'
+import Dashboard from "./Dashboard";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Employee from './Employee'
+import Profile from './Profile'
+import Home from './Home'
+import AddEmployee from './AddEmployee'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Dashboard />}>
+        <Route path='' element={<Home />}></Route>
+        <Route path='/Employee' element={<Employee />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+        <Route path='/create' element={<AddEmployee />}></Route>
+      </Route>
+      <Route path='/login' element={<Login />}></Route>
+   </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
